b8166e8aa123a345c77767d5095b8e91645e9698
Ashish-Sahu12 <ashishsahu1905@gmail.com>
2025-05-17 16:43:00 +0530

    url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:9000'
+// export const BASE_URL = 'http://localhost:9000'
+export const BASE_URL = 'https://notes-backend-sandy.vercel.app/'
 
 // export const BASE_URL = 'https://note-backend-app.vercel.app/?vercelToolbarCode=xV4BBst6d_UpbpQ'
 
